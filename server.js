985a2ff798b1b3c587ce0c72bce5f1b9d1aa2c03
AnushaA05 <anushaavinash59@gmail.com>
2025-07-21 12:49:54 +0530

    Adding the file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Routes
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
